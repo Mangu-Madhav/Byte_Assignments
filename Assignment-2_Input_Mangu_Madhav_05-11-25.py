a6615fbcf5175de6812652e228aecd596d936e74
MANGU MADHAV <madhavmangu1990@gmail.com>
2025-11-06 20:25:40 +0530

    finished the assignments

---
+++ Assignment-2_Input_Mangu_Madhav_05-11-25.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 1 Take two numbers as input and print their sum.
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=a+b
+print("sum of the numbers is:",c)
+
+
+# In[2]:
+
+
+#2 Take a float number from the user and print
+a=float(input("enter a float value"))
+print(a)
+
+
+# In[3]:
+
+
+#3  Take your age as input and print how many years are left to reach 100.
+age=int(input("enter your age"))
+print(100-age, "more years to reach 100")
+
+
+# In[5]:
+
+
+#4 Ask the user for their first and last name and print full name.
+first_name=input("enter first name")
+second_name=input("enter second_name")
+print(first_name,second_name)
+
+
+# In[9]:
+
+
+# 5 Take radius as input (float) and print the area of the circle using Area = r² (use =3.14).
+r=float(input("enter radius of the circle"))
+area=3.14*(r**2)
+print("area of the circle is:",area)
+
+
+# In[10]:
+
+
+#6 Take a number and multiply it with 2 and print their product.
+num=int(input("enter a number"))
+product=2*num
+print(f'product of {num} and 2 is {product}')
+
+
+# In[59]:
+
+
+# 7 Input a math expression as string and evaluate it (Example: 5*6*2).
+a=input("enter an expression to evaluate")
+#Not Possible from the concepts we learned
+
+
+# In[13]:
+
+
+#8  Take a string input representing a number, convert it to int, and add 10 to it
+a=input("enter a number")
+b=int(a)+10
+print(b)
+
+
+# In[15]:
+
+
+# 9 Take a float as input, convert it to integer and print both values.
+a=float(input("enter a float number"))
+b=int(a)
+print(f'float value is {a} and int value is {b}')
+
+
+# In[16]:
+
+
+# 10 program to Ask the user for their name and print: Welcome,
+name=input("enter your name")
+print(f'Welcome, {name}')
+
+
+# In[18]:
+
+
+#11 program to Add and print the result (a=12, b=5)
+a=float(input("enter a number"))
+b=float(input("enter another number"))
+result=a+b
+print(result)
+
+
+# In[19]:
+
+
+# 12 program to Multiply x * y and then subtract (x=10, y=3).
+x,y=10,3
+multiple=x*y
+subtraction=x-y
+print(f' muliplication of {x} and {y} is {multiple} and subtraction is {subtraction}')
+
+
+# In[21]:
+
+
+#13 program to Check and print the datatype of x if x is True.
+a=input("enter x value")
+x=bool(a)
+if (x==True):
+    print(x)
+
+
+# In[23]:
+
+
+#14 Program to Find remainder when one number is divided by another (17 and 4).
+a=float(input("enter a number"))
+b=float(input("enter another number"))
+remainder=a%b
+print(remainder)
+
+
+# In[24]:
+
+
+#15 Program to Multiply 3 numbers and subtract last (2, 3, and 4).
+a,b,c=2,3,4
+product=a*b*c
+sub=a-b-c
+print(f'multiplication of {a},{b},{c} is {product} and subtraction is {sub}')
+
+
+# In[26]:
+
+
+#16 Program to Form a number from digits 5, 6, 9.
+a=5
+b=6
+c=9
+print("number formed is",a+b*c)
+
+
+# In[27]:
+
+
+#17 Program to Find the difference between two integers (a=10, b=25).
+a=int(input("enter first integer"))
+b=int(input("enter second integer"))
+diff=a-b
+print("differnce is ",diff)
+
+
+# In[31]:
+
+
+#18 Program to Add 1 to a given number 99
+a=int(input("enter a number"))
+a+=1
+print("after adding 1, the new number is",a)
+
+
+# In[32]:
+
+
+#19 Program to Square a number and Add 1 (number=4).
+a=int(input("enter a number"))
+b=(a**2)+1
+print("after squaring and adding 1 to ",a," is ",b)
+
+
+# In[33]:
+
+
+#20 Program to Multiply a number by 10 and Subtract 5 (number=6).
+a=float(input("enter a number"))
+b=a*10-5
+print(b)
+
+
+# In[34]:
+
+
+#21 Program to Add two float numbers 1.234 and 2.456
+a=float(input("enter a float number"))
+b=float(input("enter another float number"))
+print(f'addition of {a} , {b} is {a+b}')
+
+
+# In[36]:
+
+
+#22 Program to Calculate the area of a circle with radius 2.5 (=3.14).
+a=2.5
+area=3.14*a**2
+print(area)
+
+
+# In[37]:
+
+
+#23 Multiply a float 2.5 with large int 1000 
+a=2.5
+b=1000
+print(a*b)
+
+
+# In[38]:
+
+
+#24  Program to Add float 2.5 with int 4. What is the output?
+# Ans: 6.5
+# proof
+a-2.5
+b=4
+print(a+b)
+
+
+# In[42]:
+
+
+#25 Program to Calculate average value of three float values 2.5, 3.5 and 7.0.
+a,b,c=float(input("enter any 3 float values to add them")),float(input()),float(input())
+avg=(a+b+c)/3
+print(avg)
+
+
+# In[45]:
+
+
+#26 Program to check  datatypes of the Given two variables 5 and 8.0,
+a=5
+b=8.0
+print(a,type(a),"\n",b,type(b))
+
+
+# In[49]:
+
+
+#27 Program to Multiply an Integer 4 with float 2.0.
+
+a=int(input("enter an integer number"))
+print(a*2.0)
+
+
+# In[51]:
+
+
+#28 Program to Check Type before and after dividing it by 2 (number=5).
+a=5
+print(a, type(a))
+      
+div=a/5
+print(div,type(div))
+
+
+# 
+
+# In[53]:
+
+
+# 29 Program to Check the boolean value of x (x=20)
+x=20
+y=bool(x)
+print(y)
+
+
+# In[54]:
+
+
+# 30 Program to Check the boolean value of x (x=0).
+x=0
+y=bool(x)
+print(x)
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment_1_Data_Types_sMangu_Madhv_04-11-25.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# 1 Program to print the number 10
+a=10
+print(a)
+
+
+# In[9]:
+
+
+# 2 program to print the numbers 5,10,15 with space
+a=5
+b=10
+c=15
+print(a,b,c)
+
+
+# In[20]:
+
+
+# 3 Program to store and print its value
+m="madhav"
+print(m)
+
+
+# In[12]:
+
+
+# program to print first and last name using two variables
+a="Mangu"
+b="Madhav"
+print(a+" "+b)
+
+
+# In[21]:
+
+
+# 5 program to print  I am 25 years old.
+a=25
+print("I am",a,"years old")
+
+
+# In[22]:
+
+
+# 6 program to print the message: My name is Madhav and I live in Narayanpet using the format() method
+
+a="madhav"
+b="Narayanpet"
+print("My name is {} and I live {}".format(a,b))
+
+
+# In[23]:
+
+
+# 7 Program to print a,b,c values which are seperated by |
+a=10
+b=20
+c=30
+print(a,"|",b,"|",c)
+
+
+# In[24]:
+
+
+# 8 Python program to print Python and Rocks! on the same line without using two print() statements.
+print("Python and Rocks!")
+
+
+# In[29]:
+
+
+#  9 program to store two numbers and print their sum using an f-string
+a=5
+b=8
+c=a+b
+print(f'sum of {a} and {b} is {c}')
+
+
+# In[31]:
+
+
+# 10 program to Ask the user to enter their name and print: Hell0, Madhav
+a=input("enter your name")
+print("Hello,",a)
+
+
+# In[32]:
+
+
+# 11 program to Create a variable named city and assign your favourite city name to it. Print the variable
+city="Hyderabad"
+print(city)
+
+
+# In[34]:
+
+
+# 12 program to swap a and b
+a=10
+b=20
+c=a
+a=b
+b=c
+print(a,b)
+
+
+# In[76]:
+
+
+# 13 program to Create three variables in one line and assign them the values 5, 10, and "Python".
+a,b,c=5,10,"python"
+print(a,b,c)
+
+
+# In[42]:
+
+
+# 14 program Assign the same value "Red" to three variables color1, color2, and color3.
+color1=color2=color3="RED"
+
+
+# In[43]:
+
+
+#15 program to Check the type of variable num that has value 3.14 and print it.
+num =3.14
+print(num, type(num))
+
+
+# In[44]:
+
+
+#16 program to Create a variable name with your name, and another variable age with your age. Print a sentence like:"My name is John and I am 25 years old."
+name="Madhav"
+age=25
+print(f'My name is {name} and I am {age} years old')
+
+
+# In[46]:
+
+
+#17 program What will be the output of the following code?
+        # a= "5"
+        # b=3
+        # print(a * b)
+a="5"
+b=3
+print( a * b) # it prints the 5's as 555
+
+
+# In[47]:
+
+
+#18  program to Create a variable is_python_fun and assign it the boolean value True. Print it.
+
+is_python_fun=True
+print(is_python_fun)
+
+
+# In[48]:
+
+
+#19 program  What is the output of the x?
+          # x = 10, x +=5
+        # Expected answer 15
+x=10
+x+=5
+print(x)
+
+
+
+# In[49]:
+
+
+#20 Can you assign the value 5 to three variables a, b, and c in one line? Then, modify only b to 10 without
+         #changing a and c.
+a=b=c=5
+b=10
+print(a,b,c)
+
+
+# In[51]:
+
+
+# 21 program to Assign 0 to five variables in one line, then update the second.
+a=b=c=d=e=0
+b=5
+print(a,b,c,d,e)
+
+
+# In[52]:
+
+
+# 22 Program Delete the variable x. where x = 10.
+
+
+
+# In[53]:
+
+
+#23 program to Create two variables x = 5 and y = 7, and print their sum.
+x=5
+y=7
+print("Sum of",x,"and",y,"is",x+y)
+
+
+# In[58]:
+
+
+#24 Program to Swap the values of the two variables;
+a=5
+b=10
+c=a
+a=b
+b=c
+print(a,b)
+
+
+# In[67]:
+
+
+# 25 program to Create a variable greeting with value "Hello" and print it.
+a="Hello "
+print(a)
+
+
+# In[68]:
+
+
+##w 26 program to Assign the result of 5 * 8 to a variable result and print it.
+result = 5*8
+print(result)
+
+
+# In[77]:
+
+
+#27 Assign two values to two variables in one line.
+a,b=10,20
+
+
+# In[72]:
+
+
+#28 program to Assign your first name and last name to two variables, then print them in one line.
+a="Mangu"
+b="Madhav"
+print(a,b)
+
+
+# In[73]:
+
+
+#29 program to Create a variable message and assign it "Good Morning". Now delete message. Try to print it after deletion.
+message="Good Morning"
+message=""
+print(message)
+
+
+# In[75]:
+
+
+#30 program to Assign a number to a variable num. Now, update num by adding 5 to it. Print the new value of num.
+num=10
+num+=5
+print(num)
+
+
+# In[ ]:
+
+
+
+
