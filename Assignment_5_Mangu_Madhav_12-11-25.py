6e09283416ef469e04f183c72ee5dbf7a2cea1bb
MANGU MADHAV <madhavmangu1990@gmail.com>
2025-11-12 11:05:27 +0530

    finsished the Assignment

---
+++ Assignment_5_Mangu_Madhav_12-11-25.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#1 Generate: 0 1 2 3 4 5 6 7 8 9
+#approach 1
+r=range(10)
+for val in r:
+    print(val)
+print("=======")
+#appraoch 2
+for val in range(10):
+    print(val)
+print("=======")
+#approach 3
+for val in range(0,10,1):
+    print(val)
+
+
+
+# In[4]:
+
+
+#2 Generate: 10 11 12 13 14 15 16 17 18 19 20
+#approach 1
+r=range(10,21)
+for val in r:
+    print(val)
+print("=======")
+#appraoch 2
+for val in range(10,21):
+    print(val)
+print("=======")
+#approach 3
+for val in range(10,21,1):
+    print(val)
+
+
+# In[5]:
+
+
+#3 Generate 100 101 102 103 104 105
+#Approach 1
+r=range(100,106)
+for val in r:
+    print(val)
+print("=======")
+
+#approach 2
+for val in range(100,106,1):
+    print(val)
+
+
+# In[12]:
+
+
+#4 Generate -10 -11 -12 -13 -14 -15 -16 
+
+#Appraoch 1
+r=range(-10,-17,-1)
+for val in r:
+    print(val)
+
+print("=======")
+
+#approach 2
+for val in range(-10,-17,-1):
+    print(val)
+
+
+# In[13]:
+
+
+# 5 Generate 10 9  8 7 6 5 4 3 2 1
+#Approach 1
+print("=======")
+r=range(10,0,-1)
+for val in r:
+    print(val)
+print("=======")
+for val in range(10,0,-1):
+    print(val)
+
+
+# In[16]:
+
+
+# 6 Generate: -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+#Approach 1
+r=range(-10,0)
+for val in r:
+    print(val)
+print("=======")
+for val in range(-10,0,1):
+    print(val)
+
+
+# In[1]:
+
+
+#7 Generate: -1 -2 -3 -4 -5 -6 -7 -8 -9 -10
+#approach 1
+r=range(-1,-11,-1)
+for val in r:
+    print(val)
+print("=======")
+#approach 2
+for val in range(-1,-11,-1):
+    print(val)
+
+
+# In[22]:
+
+
+#8 Generate: 10 12 14 16 18 20
+#Appraoch 1
+r=range(10,21,2)
+for val in r:
+    print(val)
+print("=======")
+#approach 2
+for val in range(10,21,2):
+    print(val)
+
+
+
+# In[24]:
+
+
+#9 Generate: 100 110 120  130 140 150 
+#approach 1
+r=range(100,151,10)
+for val in r:
+    print(val)
+print("=======")
+for val in range(100,151,10):
+    print(val)
+
+
+# In[27]:
+
+
+#10 Generate: -20 -18 -16 -14 -12 -10
+#Approach 1
+r=range(-20,-9,2)
+for val in r:
+    print(val)
+print("=======")
+for val in range(-20,-9,2):
+    print(val)
+
+
+# In[28]:
+
+
+# 11 Generate: -1000 -900 -800 -700 -600 -500
+r=range(-1000,-400,100)
+for val in r:
+    print(val)
+print("=======")
+for val in range(-1000,-400,100):
+    print(val)
+
+
+# In[29]:
+
+
+#12 Generate: -5 -4 -3 -2 -1 0 1 2 3 4 5
+#Approach 1
+r=range(-5,6)
+for val in r:
+    print(val)
+print("=======")
+for val in range(-5,6,1):
+    print(val)
+
+
+# In[30]:
+
+
+#13 Generate 5 4 3 2 1 0 -1 -2 -3 -4 -5 
+#approach 1
+r=range(5,-6,-1)
+for val in r:
+    print(val)
+print("=======")
+for val in range(5,-6,-1):
+    print(val)
+
+
+# In[ ]:
+
+
+
+
