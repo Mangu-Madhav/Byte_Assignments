a11a000d4e8502364a352c7f7cc67d91fd667882
Mangu-Madhav <154441396+Mangu-Madhav@users.noreply.github.com>
2025-11-13 11:32:49 +0530

    Update Assgignment_4_List_Mangu_Madhav_11-11-25.py

--- Assgignment_4_List_Mangu_Madhav_11-11-25.py
+++ Assgignment_4_List_Mangu_Madhav_11-11-25.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # In[73]:
-
+#add 10 to all elements in the list l1=[10,11,13,14]
 
 l1=[10,11,13,14]
 print("list 1",l1)
@@ -13,7 +13,7 @@ print("list-2",l2)
 
 
 # In[72]:
-
+#store the resultant of l1+l2 in l3 l1=[10,11,13,14] , l2=[5,6,7,8,9]
 
 l1=[10,11,13,14]
 l2=[5,6,7,8,9]
@@ -27,7 +27,7 @@ print("list-3",l3)
 
 
 # In[60]:
-
+#Extract the elements >=40 in the given list l1=[10,11,23,45,78,90,42,41]
 
 l1=[10,11,23,45,78,90,42,41]
 print("list-1",l1)
@@ -39,7 +39,7 @@ print("list-2",l2)
 
 
 # In[64]:
-
+#Extract the values of the indices [0,1,5,7] from the list l1=[10,20,15,45,89,90,11,34]
 
 l1=[10,20,15,45,89,90,11,34]
 print("list-1",l1)
