bab11a80447e43c4466bf0b4bf77fc618b8cac71
MANGU MADHAV <madhavmangu1990@gmail.com>
2025-12-03 15:08:24 +0530

    finished Assignment

--- Assgignment_12_Pandas_Mangu_Madhav_02-12-25.py
+++ Assgignment_12_Pandas_Mangu_Madhav_02-12-25.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[57]:
+# In[102]:
 
 
 # Assignment: Data Selection, Modification, and Operations in Pandas
@@ -22,14 +22,14 @@ b=a.reshape(10,5)
 print(b)
 df=pd.DataFrame(b,columns=["A","B","C","D","E"])
 print("DataFrame:\n",df)
-# a
+a=df
 print("The value at the 3rd row and 2nd column:",b[2,1])
 print("all rows but only columns ['A', 'C']\n",df.loc[:,["A","C"]])
 print("multiple rows [2, 4, 7] and all columns\n",df.iloc[[2,4,7],:])
 print("rows [1, 3, 5] and columns ['B', 'D']\n",df.loc[[1,3,5],["B","D"]])
 
 
-# In[58]:
+# In[103]:
 
 
 # Part B: Data Modification
@@ -52,11 +52,11 @@ df.loc[[2,5],"A"]=99
 print("Replaced rows [2,5] in column A with 99\n",df)
 df.loc[[0,1,2]]=[[1,1,1,1,1],[2,2,2,2,2],[3,3,3,3,3]]
 print("Replace rows [0,1,2] with [ [1,1,1,1,1], [2,2,2,2,2], [3,3,3,3,3] ]\n",df)
-df=df.drop(columns="B")
-df
+print("drop index B\n",df.drop(columns="B"))
+a
 
 
-# In[61]:
+# In[104]:
 
 
 # Part C: Deleting Values
@@ -73,6 +73,67 @@ print("Drop column D:\n",df.drop(columns="D"))
 print("Drop multiple columns ['B', 'E']:\n",df.drop(columns="E"))
 print("Drop multiple rows [0,2,5]:\n",df.drop(index=[0,2,5]))
 df
+
+
+# In[105]:
+
+
+# Part D: Mathematical Operations
+# 4.	a. Add 10 to all values in column A.
+# b. Multiply column B by 2.
+# c. Calculate the sum of row 5.
+# d. Calculate the mean of all columns.
+print(df)
+df["A"]+=10
+print("Added 10 to all values in column A\n",df)
+df["B"]*=2
+print("Multiply column B by 2\n",df)
+# print("Calculate the sum of row 5",df.sum(index=5))
+
+
+# In[108]:
+
+
+# Part E: Conditional Selection
+# 5.	a. Extract all rows where column A > 50.
+# b. Extract all rows where column B < 30 and column D > 10.
+# c. Replace all values in column C where value < 50 with 0.
+print(a)
+print("Extract all rows where column A > 50\n",a[a["A"]>50])
+print("Extract all rows where column B < 30 and column D > 10\n",a[(a["B"]<30) & (a["D"]>10)])
+# 
+# a[a.loc[:,"C"]<50]=0
+a.loc[a["C"] < 20, "C"] = 0
+print("Replace all values in column C where value < 20 with 0\n",a)
+
+
+# In[107]:
+
+
+# Part F: Bonus / Advanced
+# 6.a. Replace multiple rows and multiple columns with a single value (e.g., replace rows [1,2] and columns ['B','D'] with 999).
+# 7.b. Replace multiple rows and multiple columns with different values.
+# 8.c. Extract values using iloc and loc for specific positions and labels.
+print(a)
+a.loc[[1,2],["B","D"]]=999
+print("\nReplace multiple rows and multiple columns with a single value (e.g., replace rows [1,2] and columns ['B','D'] with 999)")
+print(a)
+a.loc[[1,3,5],["A","C"]]=[[1,3],[109,95],[3,5]]
+print("Replace multiple rows and multiple columns with different values\n",a)
+print("using loc:\n",a.loc[[1,3,7],["C","E"]])
+print("using iloc:\n",a.iloc[[1,6,9]])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
