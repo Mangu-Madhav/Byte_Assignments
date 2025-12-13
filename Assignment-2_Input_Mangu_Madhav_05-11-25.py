1ff0a4b9fb57d6c105ad9a9ed5fb3bad318c7815
Mangu-Madhav <154441396+Mangu-Madhav@users.noreply.github.com>
2025-12-13 20:12:10 +0530

    Update Assignment-2_Input_Mangu_Madhav_05-11-25.py

--- Assignment-2_Input_Mangu_Madhav_05-11-25.py
+++ Assignment-2_Input_Mangu_Madhav_05-11-25.py
@@ -39,7 +39,7 @@ print(first_name,second_name)
 # In[9]:
 
 
-# 5 Take radius as input (float) and print the area of the circle using Area = r² (use =3.14).
+#5 Take radius as input (float) and print the area of the circle using Area = r² (use =3.14).
 r=float(input("enter radius of the circle"))
 area=3.14*(r**2)
 print("area of the circle is:",area)
