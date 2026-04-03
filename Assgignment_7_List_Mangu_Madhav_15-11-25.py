d02fdce679a662dbe2a1006858af810980f9aaab
MANGU MADHAV <madhavmangu1990@gmail.com>
2026-04-03 10:13:24 +0530

    updated

--- Assgignment_7_List_Mangu_Madhav_15-11-25.py
+++ Assgignment_7_List_Mangu_Madhav_15-11-25.py
@@ -45,7 +45,6 @@ print(l2)
 
 # In[19]:
 
-
 ### 5: Filtering Lists
 #Create a new list containing only the even numbers from the list created in Assignment 1 using a list comprehension. Print the new list.
 l1=list(range(1,21))
